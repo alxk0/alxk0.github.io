e59431621e011e8b818a876ae8c04cecfd9f32af
ALXK <98492373+alxk0@users.noreply.github.com>
2022-11-19 20:20:24 -0500

    alx

---
+++ js/carousel/multiple.js
@@ -0,0 +1,24 @@
+/* https://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+let slideIndex = [1,1,1,1];
+let slideId = ["slides00", "slides02", "slides03", "slides04"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
