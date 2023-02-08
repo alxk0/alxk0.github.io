e8740a3c049ea0070a518514ffb31715718b747b
ALXK <98492373+alxk0@users.noreply.github.com>
2023-02-08 11:02:05 -0500

    z

---
+++ muse/00/sketch.js
@@ -0,0 +1,38 @@
+
+// KUZUSHIJI-MNIST
+// CTC-1000-04 : INTRODUCTION TO COMPUTATION
+// ALEX KIM, 08 FEB 2023
+
+let folder = "images/";
+let files = [];
+let images = [];
+let gridSize = 4;
+let cellSize;
+let currentImage = 0;
+
+function preload() {
+  for (let i = 0; i < 5; i++) {
+    files.push(i + ".png");
+    images[i] = loadImage(folder + files[i]);
+  }
+}
+
+function setup() {
+  createCanvas(600, 600);
+  cellSize = width / gridSize;
+  frameRate(15);
+}
+
+function draw() {
+  background(255);
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let x = i * cellSize;
+      let y = j * cellSize;
+      let index = i + j * gridSize + currentImage * gridSize * gridSize;
+      index = index % images.length;
+      image(images[index], x, y, cellSize, cellSize);
+    }
+  }
+  currentImage = (currentImage + 1) % (images.length / (gridSize * gridSize));
+}
