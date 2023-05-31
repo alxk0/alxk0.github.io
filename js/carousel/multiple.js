e8d7056691dcee8fb29a816593081b72493b61d6
ALXK <98492373+alxk0@users.noreply.github.com>
2023-05-31 09:05:04 -0400

    alxk

--- js/carousel/multiple.js
+++ js/carousel/multiple.js
@@ -1,7 +1,7 @@
 /* https://www.w3schools.com/howto/howto_js_slideshow.asp */
 
 let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-let slideId = ["slides00","slides02","slides03","slides04","slides06","slides07", "slides08","slides09","slides10","slides11","slides12","slides13","slides15","slides18"]
+let slideId = ["slides01","slides03","slides04","slides05","slides07", "slides08","slides09","slides10","slides11","slides12","slides13","slides14","slides16","slides19"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
